76e41366db4592dd72f1431d30b2d7ec458c2db9
wujiazhen2 <1599153675@qq.com>
2019-03-08 00:36:46 +0800

    fix:修改抽象工厂,增加工厂模式理解笔记

---
+++ 设计模式/工厂模式/code/src/main/java/com/wjz/learnjava/abstractFactory/AbstractMotorbike.java
@@ -0,0 +1,9 @@
+package com.wjz.learnjava.abstractFactory;
+
+/**
+ * @author wujiazhen
+ * @date 2019/3/8
+ */
+public abstract class AbstractMotorbike {
+    public abstract void use();
+}
---
+++ 设计模式/工厂模式/code/src/main/java/com/wjz/learnjava/abstractFactory/AudiMotorbike.java
@@ -0,0 +1,11 @@
+package com.wjz.learnjava.abstractFactory;
+
+/**
+ * @author wujiazhen
+ * @Date 2019/3/7
+ */
+public  class AudiMotorbike extends AbstractMotorbike {
+    public void use() {
+        System.out.println("奥迪车摩托车启动。。。。。。。。。。。");
+    }
+}
